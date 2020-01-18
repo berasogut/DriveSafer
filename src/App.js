956f74ecd2522ccfa79013346097534fa8f92459
Hani Damlaj <hanidamlaj@Hanis-MacBook-Pro.local>
2020-01-18 15:57:16 -0500

    init repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const onLoad = () => {
+		const frame = document.getElementById("maps_frame");
+		console.log(frame);
+		frame.width = document.body.clientWidth;
+		// frame.style.height = document.body.clientWidth;
+	};
+
+	return (
+		<iframe
+			id="maps_frame"
+			width="600"
+			height="450"
+			frameBorder="0"
+			onLoad={onLoad}
+			// style="border:0"
+			src="https://www.google.com/maps/embed/v1/view?zoom=10&center=45.5017%2C-73.5673&key=AIzaSyAzFZb4XxRJd-mmin250NYxfHo-kjkj_E4
+"
+			allowFullScreen
+		></iframe>
+	);
 }
 
 export default App;
